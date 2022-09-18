637f65b146cbdbf136c4697c83f27d713d1c8a96
Akarui <60879671+AkaruiAnims@users.noreply.github.com>
2022-09-18 22:15:40 +0200

    more basics

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+// Entry point for index.html i suppose 
+
+// Note: make it an aestetic website with rain and lofi themes
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
